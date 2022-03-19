ad76f2c941ec47de504573842f59cf1f66da2737
madyfabs <fabriciio_@live.com>
2022-03-19 17:46:57 -0400

    Finalizado

---
+++ ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace ProjetoElevador.Model
+{
+    internal class Elevador
+    {
+
+        //Construtor sem parametros, apenas para inicializar todas as variaveis 
+        public Elevador()
+        {
+            this.andarAtual = 0;
+            this.totalAndares = 0;
+            this.capacidadeElevador = 0;
+            this.capacidadeAtual = 0;
+        }
+
+        //Variaveis utilizadas no programa.
+        public int andarAtual { get; set; }
+        public int totalAndares { get; set; }
+        public int capacidadeElevador{ get; set; }
+        public int capacidadeAtual { get; set; }
+                
+
+        //Método Inicializar:
+        //Captura a capacidade máxima do elevador e o total de andares do prédio
+        //através de mensagens amigáveis ao usuário.
+        public void Inicializar()
+        {
+            Console.WriteLine(" Insira a capacidade máxima do elevador:");
+            Console.Write(" Sua Opcao: ");
+            capacidadeElevador = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Insira o total de andares do prédio:");
+            Console.Write(" Sua Opcao: ");
+            totalAndares = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+        }
+
+        ///Método Entrar:
+        //Método que adiciona uma pessoa ao elevador, desde que o mesmo
+        //NÃO esteja na sua capacidade máxima
+        //Caso esteja cheio, é exibida uma mensagem informando ao usuário deste fato.
+        public void Entrar()
+        {
+            if(capacidadeAtual < capacidadeElevador)
+            {
+                capacidadeAtual++;
+            }
+            else
+            {
+                Console.WriteLine(" Não é possível entar. Elevador está cheio!");
+            }
+        }
+
+        ///Método Sair:
+        //Método que remove uma pessoa ao elevador, desde que o mesmo
+        //NÃO esteja vazio
+        //Caso esteja vazio, é exibida uma mensagem informando ao usuário deste fato.
+        public void Sair()
+        {
+            if (capacidadeAtual > 0)
+            {
+                capacidadeAtual--;
+            }
+            else
+            {
+                Console.WriteLine(" Não é possível sair. Elevador está vazio!");
+            }
+        }
+
+
+        ///Método Subir:
+        //Método que sobe o elevador em um andar, desde que
+        //NÃO esteja no ultimo andar (andar máximo)
+        //Caso esteja no último andar, é exibida uma mensagem informando ao usuário deste fato.
+        public void Subir()
+        {
+            if (andarAtual < totalAndares)
+            {
+                andarAtual++;
+            }
+            else
+            {
+                Console.WriteLine(" Não é possível subir outro andar. Elevador já está no topo do prédio!");
+            }
+
+        }
+
+        ///Método Descer:
+        //Método que desce o elevador em um andar, desde que
+        //NÃO esteja no térreo (andar mínimo)
+        //Caso esteja no terreo, é exibida uma mensagem informando ao usuário deste fato.
+        public void Descer()
+        {
+            if (andarAtual > 0)
+            {
+                andarAtual--;
+            }
+            else
+            {
+                Console.WriteLine(" Não é possível descer outro andar. Elevador já está no térreo do prédio!");
+            }
+        }
+
+        ///Método ToString:
+        //Método que ao final da execução do programa,
+        //imprime um "resumo" de onde o elevador parou (andar) e 
+        //sua capacidade atual (pessoas), juntamente com a quantidade de andares máximos e capacidade máxima que foram inseridos pelo usuário.
+        public override string ToString()
+        {
+            return ($"O Elevador do prédio de {totalAndares} andar/andares, com capacidade para {capacidadeElevador} pessoas, finalizou no andar de número {andarAtual} e com {capacidadeAtual} pessoa/pessoas dentro!");
+        }
+
+    }
+}
---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ProjetoElevador.Model;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //A variavel operacaoUsuario é utilizada no menu interativo para capturar qual ação a ser realizada
+            //A variavel validaOperacoes é utilizada também no menu interativo para capturar apenas a ação de término do programa.
+            string operacaoUsuario;
+            bool validaOperacoes = true;
+
+            //Variavel elevador sendo instanciada para podermos utilizar seus métodos.
+            Elevador elevador = new Elevador();
+
+            //Aqui é chamado o método Inicializar() que captura a capacidade do elevador e a quantidade de andares do prédio
+            elevador.Inicializar();
+
+            //Aqui começa o menu interativo, que será sempre repetido até que o usuario escolha a opção 5, que finaliza o programa.
+            do
+            {
+                Console.WriteLine(" Escolha uma opção:");
+                Console.WriteLine(" 1 - Entrar no Elevador");
+                Console.WriteLine(" 2 - Sair do Elevador");
+                Console.WriteLine(" 3 - Subir Andar");
+                Console.WriteLine(" 4 - Descer Andar");
+                Console.WriteLine(" 5 - Finalizar Programa");
+
+                Console.Write(" Sua Opcao: ");
+                operacaoUsuario = Console.ReadLine();
+                Console.Clear();
+
+                //Esse escolha determina qual operação será utilizada a partir da entrada do usuário acima, através da variavel operacaoUsuario.
+                switch (operacaoUsuario)
+                {
+                    case "1": Console.WriteLine(" Você escolheu: Entrar"); elevador.Entrar(); break;
+                    case "2": Console.WriteLine(" Você escolheu: Sair"); elevador.Sair(); break;
+                    case "3": Console.WriteLine(" Você escolheu: Subir"); elevador.Subir(); break;
+                    case "4": Console.WriteLine(" Você escolheu: Descer"); elevador.Descer(); break;
+                    case "5": validaOperacoes = false; break;
+                    default: Console.WriteLine("Escolha não válida."); break;
+                }
+                Console.WriteLine("");
+            }
+
+            while (validaOperacoes);
+
+            //Após o usuário decidir terminar a execução do programa, é impresso na tela o andar que o elevador ficou e a quantidade de pessoas dentro dele.
+            Console.WriteLine(elevador.ToString());
+            
+        }
+
+    }
+}
